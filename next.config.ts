223a252a6ac6e42f26cab6c6413fd0427ff7fff8
Schyler Manning <schyler.manning@gmail.com>
2026-01-13 09:37:12 -0500

    Fix build configuration: add webpack flag, remove legacy seo.ts, add next-pwa types

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Use webpack for builds since next-pwa requires it
+  webpack: (config, { isServer }) => {
+    return config;
+  },
+  // Fix warning about multiple lockfiles
+  outputFileTracingRoot: require('path').join(__dirname),
 };
 
 const pwaConfig = withPWA({
